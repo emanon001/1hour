f7046e8484bb6121b6a04a73d021ea2d8bd5a0ba
emanon001 <emanon001@gmail.com>
2022-10-17 03:13:39 +0900

    refactor: シンプルな実装に変更

--- 003-e1tree/src/main.rs
+++ 003-e1tree/src/main.rs
@@ -9,65 +9,40 @@ struct Opt {
     directory: String,
 }
 
-enum TreeItem<'a> {
-    Root(&'a Path),
-    DirectoryEntry {
-        path: &'a Path,
-        is_last_entry: bool,
-        line_prefix: &'a String,
-    },
-}
+fn build_tree_lines(path: &Path) -> Result<Vec<String>> {
+    let mut result = Vec::new();
 
-fn print_tree(item: TreeItem) -> Result<()> {
-    let (line_string, dir_context): (String, Option<(&Path, String)>) = match item {
-        TreeItem::Root(path) => {
-            let file_name = path.file_name().unwrap().to_str().unwrap();
-            (file_name.to_owned(), Some((path, "".to_owned())))
-        }
-        TreeItem::DirectoryEntry {
-            path,
-            is_last_entry,
-            line_prefix,
-        } => {
-            let file_name = path.file_name().unwrap().to_str().unwrap();
-            let file_prefix = if is_last_entry {
-                "└── "
-            } else {
-                "├── "
-            };
-            let line = format!("{}{}{}", line_prefix, file_prefix, file_name);
-            let dir_context = if path.is_dir() {
-                let new_line_prefix = format!(
-                    "{}{}",
-                    line_prefix,
-                    if is_last_entry { "    " } else { "│   " }
-                );
-                Some((path, new_line_prefix))
-            } else {
-                None
-            };
-            (line, dir_context)
-        }
-    };
+    let file_name = path.file_name().unwrap().to_str().unwrap().to_string();
+    result.push(file_name);
 
-    // print path line
-    println!("{}", line_string);
-
-    // print directory entries
-    if let Some((dir, line_prefix)) = dir_context {
-        let mut entries = fs::read_dir(dir)?
+    if path.is_dir() {
+        let mut entries = fs::read_dir(path)?
             .map(|res| res.map(|e| e.path()))
             .collect::<Result<Vec<_>, std::io::Error>>()?;
         entries.sort();
         for (i, path) in entries.iter().enumerate() {
-            print_tree(TreeItem::DirectoryEntry {
-                path: path.as_path(),
-                is_last_entry: i == entries.len() - 1,
-                line_prefix: &line_prefix,
-            })?;
+            let is_last_entry = i == entries.len() - 1;
+
+            let lines = build_tree_lines(path)?;
+            assert!(!lines.is_empty());
+            let (child, descendants) = lines.split_at(1);
+
+            // child
+            let prefix = if is_last_entry {
+                "└── "
+            } else {
+                "├── "
+            };
+            result.push(format!("{}{}", prefix, child[0]));
+
+            // descendants (excluded child)
+            let prefix = if is_last_entry { "    " } else { "│   " };
+            for e in descendants {
+                result.push(format!("{}{}", prefix, e));
+            }
         }
     }
-    Ok(())
+    Ok(result)
 }
 
 fn main() -> Result<()> {
@@ -77,6 +52,9 @@ fn main() -> Result<()> {
         return Err(anyhow!("argument is not directory"));
     }
     let path = Path::new(&opt.directory);
-    print_tree(TreeItem::Root(path))?;
+    let lines = build_tree_lines(path)?;
+    for l in lines {
+        println!("{}", l);
+    }
     Ok(())
 }
