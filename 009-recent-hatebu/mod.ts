753703b87d5aa2f34ca38bf94821591f837eae65
emanon001 <emanon001@gmail.com>
2022-10-29 16:06:14 +0900

    009: feat: HTMLのパース処理を追加

--- 009-recent-hatebu/mod.ts
+++ 009-recent-hatebu/mod.ts
@@ -1,3 +1,8 @@
+import {
+  DOMParser,
+  Element,
+} from "https://deno.land/x/deno_dom@v0.1.35-alpha/deno-dom-wasm.ts";
+
 const fetchHatebuHtml = async (id: string): Promise<string> => {
   const url = `https://b.hatena.ne.jp/${id}/bookmark`;
   const resp = await fetch(url);
@@ -5,9 +10,21 @@ const fetchHatebuHtml = async (id: string): Promise<string> => {
 };
 
 // TODO: parse HTML
-type HatebuEntry = {};
+type HatebuEntry = {
+  url: string;
+  text: string;
+};
 const parseHatebuHtml = (html: string): HatebuEntry[] => {
-  return [];
+  const doc = new DOMParser().parseFromString(html, "text/html")!;
+  const entryList = Array.from(
+    doc.querySelectorAll(".bookmark-item"),
+  ) as Element[];
+  return entryList.map((elm) => {
+    const titleElem = elm.querySelector(".centerarticle-entry-title a")!;
+    const url = titleElem.getAttribute("href")!;
+    const text = titleElem.textContent;
+    return { url, text };
+  });
 };
 
 // TODO: print hatebu entry list
